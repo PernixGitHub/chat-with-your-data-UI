3858616d28f80630e1962760c9f5e26b524387e9
paramveer-pernix <paramveers@pernix.com.au>
2025-10-21 01:12:10 +1030

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Install dependencies
+echo "Installing dependencies..."
+npm install
+
 # Start the frontend
+echo "Starting frontend..."
 npm run dev &
 
 # Start the backend
+echo "Starting backend..."
 cd backend; node index.js;
